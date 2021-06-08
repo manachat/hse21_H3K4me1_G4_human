2fb6d60816e44ba3a64d67b8c52a359a59d93ceb
manachat <vafilonov@edu.hse.ru>
2021-06-08 17:48:39 +0300

    filter

---
+++ src/filter.sh
@@ -0,0 +1,2 @@
+awk '$3 - $2 < 5001' ../data/H3K4me1_HCT116_ENCFF699ABD_hg19.bed > ../data/H3K4me1_HCT116_ENCFF699ABD_hg19_filtered.bed
+awk '$3 - $2 < 5001' ../data/H3K4me1_HCT116_ENCFF931YSQ_hg19.bed > ../data/H3K4me1_HCT116_ENCFF931YSQ_hg19_filtered.bed
