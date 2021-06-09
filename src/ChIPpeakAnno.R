10faa776eb1f03b0e5cdca3c7c758f4a6018e2ab
manachat <vafilonov@edu.hse.ru>
2021-06-09 22:33:26 +0300

    anno

---
+++ src/ChIPpeakAnno.R
@@ -0,0 +1,35 @@
+source("lib.R")
+
+#BiocManager::install("ChIPpeakAnno")
+library(ChIPpeakAnno)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(org.Hs.eg.db)
+#library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+#library(org.Mm.eg.db)
+
+###
+
+
+peaks <- toGRanges(paste0(DATA_DIR, 'H3K4me1_HCT116_intersect_G4.bed'), format="BED")
+peaks[1:2]
+
+annoData <- toGRanges(TxDb.Hsapiens.UCSC.hg19.knownGene)
+annoData[1:2]
+
+
+anno <- annotatePeakInBatch(peaks, AnnotationData=annoData, 
+                            output="overlapping", 
+                            FeatureLocForDistance="TSS",
+                            bindingRegion=c(-2000, 300))
+data.frame(anno) %>% head()
+
+anno$symbol <- xget(anno$feature, org.Hs.egSYMBOL)
+data.frame(anno) %>% head()
+
+anno_df <- data.frame(anno)
+write.table(anno_df, file=paste0(DATA_DIR, 'H3K4me1_HCT116_intersect_G4_genes.txt'),
+            col.names = TRUE, row.names = FALSE, sep = '\t', quote = FALSE)
+
+uniq_genes_df <- unique(anno_df['symbol'])
+write.table(uniq_genes_df, file=paste0(DATA_DIR, 'H3K4me1_HCT116_intersect_G4_genes_uniq.txt'),
+            col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
